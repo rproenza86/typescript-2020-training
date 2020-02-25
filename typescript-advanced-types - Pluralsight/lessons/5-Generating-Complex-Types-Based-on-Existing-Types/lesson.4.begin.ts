8eea224a756437c5bb91db36f897d92f2e1612f6
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-02-25 07:46:20 -0500

    folder rename

--- typescript-advanced-types - Pluralsight/lessons/5-Generating-Complex-Types-Based-on-Existing-Types/lesson.4.begin.ts
+++
@@ -1,9 +0,0 @@
-import { Project, TextLayer, ImageLayer, LayerType, Size } from "./types";
-
-const fieldDescriptions = {
-  text: "This is the default text"
-};
-
-Object.entries(fieldDescriptions).forEach(([field, description]) => {
-  console.log(`${field}: ${description}`);
-});

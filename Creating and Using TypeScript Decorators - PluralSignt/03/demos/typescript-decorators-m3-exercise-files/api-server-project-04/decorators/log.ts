1678fb15204907d6a015c59a9f74254a62dfd977
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-02-26 11:32:17 -0500

    chore: Added decorators training

---
+++ Creating and Using TypeScript Decorators - PluralSignt/03/demos/typescript-decorators-m3-exercise-files/api-server-project-04/decorators/log.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+export function logRoute(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+        let req = args[0] as Request;
+        let res = args[1] as Response;
+
+        originalMethod.apply(this, args);
+
+        console.log(`${req.ip} [${new Date().toISOString()}] ${req.host} ${req.originalUrl} ${req.method} ${res.statusCode} ${res.statusMessage} HTTP/${req.httpVersion}`);
+        if(["PUT", "POST"].indexOf(req.method) > -1) {
+            console.log(`\tBODY: ${JSON.stringify(req.body)}`);
+        }
+    }
+}
---
+++ Creating and Using TypeScript Decorators - PluralSignt/README.md
@@ -0,0 +1,3 @@
+# Creating and Using TypeScript Decorators
+
+These exercise files are intended to provide you with the assets you need to create a video-based hands-on experience. With the exercise files, you can follow along with the author and re-create the same solution on your computer. We find this to be even more effective than written lab exercises.
